bef640a26820e00757e2ab3bee323f1eb21f7fff
banmul6 <banteamlakmul6@gmali.com>
2022-12-09 12:06:11 -0800

    postive orr negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	int n
+	int n;
+
 	srand(time(0));
 	n = read() - RAND_MAX / 2;
 	if (n > 0)
@@ -17,11 +18,9 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}
 	else
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
