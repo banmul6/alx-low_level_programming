f39f45500003233fb29cdd82d075da00bb3085e5
banmul6 <banteamlakmul6@gmali.com>
2022-12-11 10:15:30 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,8 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = read() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -18,6 +19,7 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
 	else
 	{
 		printf("%d is negative\n", n);
