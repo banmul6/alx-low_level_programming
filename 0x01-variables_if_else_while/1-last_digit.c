9af5d012c6e0a9271acb158c07a30605d815ff2a
banmul6 <banteamlakmul6@gmali.com>
2022-12-12 00:41:42 -0800

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if
+		(n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+
+	else if
+		("n = 0")
+	{
+		printf("and is 0\n");
+	}
+
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
