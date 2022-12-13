09827432adb2ffcff0c612378bb405b8234db84d
banmul6 <banteamlakmul6@gmali.com>
2022-12-13 12:41:01 -0800

    print last digit

--- 0x02-functions_nested_loops/main_4.c
+++ 0x02-functions_nested_loops/main_4.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int r;
 
-	r = print_last_digit(INT_MIN);
-	_putchar('0' + r);
-	_putchar('\n');
+	r = print_last_digit(INT_MIN)
+		_putchar('0' + 'r')
+		_putchar('\n');
 	return (0);
 }
 
